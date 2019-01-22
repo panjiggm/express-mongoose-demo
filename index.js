511076669933bced81c282e95fdd27d2d096e3e5
Panji Gumelar <panjiggm@gmail.com>
2019-01-22 12:00:00 +0700

    Create POST method (input users)

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
 
-const PORT = 8000;
+const PORT = 8000 // Development Port
+
+// use body-parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Initialize mongoose
-const mongoose = require("mongoose");
-const MONGODB_URI = `mongodb://localhost:27017/company`;
+const mongoose = require("mongoose")
+const MONGODB_URI = `mongodb://localhost:27017/company`
 
 mongoose.connect(
   MONGODB_URI,
   {
     useNewUrlParser: true
   }
-);
+)
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 const User = mongoose.model(
   "users",
-  new Schema({
-    first_name: String,
-    last_name: String,
-    age: Number,
-    address: {}
-  })
-);
+  new Schema(
+    {
+      first_name: String,
+      last_name: String,
+      age: Number,
+      address: {
+        street: String,
+        city: String,
+        state: String
+      }
+    },
+    { timestamps: true }
+  )
+)
 
-app.listen(PORT, () => console.log(`App running on ${PORT}`));
+app.post("/users", (req, res) => {
+  User.create(req.body, (err, users) => {
+    if (err) return res.send("Gagal cyt")
+
+    res.send({
+      data: users
+    })
+  })
+  // res.send(req.body)
+})
+
+app.listen(PORT, () => console.log(`App running on ${PORT}`))
